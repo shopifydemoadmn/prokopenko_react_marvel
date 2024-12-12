8be526b0d68f27e6f5731c7379206099025dd58e
shopifydemoadmn <shopifydemoadmn@gmail.com>
2024-12-12 16:34:49 +0200

    add files

---
+++ src/components/app/App.js
@@ -0,0 +1,33 @@
+import {lazy} from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
+import AppHeader from "../appHeader/AppHeader";
+
+const Page404 = lazy(() => import('../pages/404'));
+const MainPage = lazy(() => import('../pages/MainPage'));
+const ComicsPage = lazy(() => import('../pages/ComicsPage'));
+const SingleComicPage = lazy(() => import('../pages/SingleComicPage'));
+
+const App = () => {
+    return (
+      <Router>
+        <div className="app">
+            <AppHeader/>
+            <main>
+              <Routes>
+                <Route element={<MainPage />} path="/" />
+                <Route element={<ComicsPage />} path="/comics" />
+                <Route element={<SingleComicPage />} path="/comics/:comicId" />
+                <Route element={<Page404 />} path="*" />
+              </Routes>
+            </main>
+        </div>
+      </Router>
+    )
+}
+
+export default App;
